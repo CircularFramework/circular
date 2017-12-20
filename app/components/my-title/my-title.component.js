18bf861e376cfc4c4771f3d36cb5d56ecb2c0634
Rick Hopkins <rick.hopkins@gmail.com>
2017-12-20 08:31:28 -0500

    Updated test my-title component

--- app/components/my-title/my-title.component.js
+++ app/components/my-title/my-title.component.js
@@ -4,9 +4,7 @@ import { CircularComponent } from '../../../src/decorators/circular-component.de
 	selector: 'my-title',
 	template: `<h1>The {{title}} Family</h1>`,
 	styleUrl: './my-title.component.scss',
-	attributes: ['title'],
-	//styles: ':host > h1 { font-size: 11px; }'
-	styleUrl: './my-title.component.scss',
+	attributes: ['title']
 })
 export class MyTitleComponent {
 	constructor() {
